e2a143f24642e5fb8230de5314c6c60e60cecd0a
Valmir <vkairy@users.noreply.github.com>
2022-08-10 15:22:37 -0300

    Removing dead code used before Redux integration

--- src/components/counter-list/counterList.jsx
+++ src/components/counter-list/counterList.jsx
@@ -1,18 +1,4 @@
 import './counterList.css'
-
-// import Counter from '../counter'
-
-// function CounterList({counters, onIncrement, onDecrement, onDelete}) {
-//     return (
-//         <div id='counter-list-container'>
-//             {counters.map(counter =>
-//                 <Counter key={counter.id} counter={counter} onIncrement={onIncrement} onDecrement={onDecrement} onDelete={onDelete} />)}
-//         </div>
-//     );
-// }
-
-// export default CounterList;
-
 import Counter from '../counter'
 import { connect } from 'react-redux';
 
--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,27 +1,5 @@
 import './counter.css'
 import { MdAddCircle, MdRemoveCircle, MdDeleteForever } from 'react-icons/md'
-
-// function Counter({counter, onIncrement, onDecrement, onDelete}) {
-//     return (
-//         <div id='counter-container'>
-//             <p id='badge' className={counter.value === 0 ? 'zero' : ''}>
-//                 {counter.value === 0 ? 'zero' : counter.value}
-//             </p>
-//             <button id='increment' onClick={() => onIncrement(counter.id)}>
-//                 <MdAddCircle />
-//             </button>
-//             <button id='decrement' onClick={() => onDecrement(counter.id)}>
-//                 <MdRemoveCircle />
-//             </button>
-//             {counter.value === 0 && <button id='delete' onClick={() => onDelete(counter.id)}>
-//                 <MdDeleteForever />
-//             </button>}
-//         </div>
-//     );
-// }
-
-// export default Counter;
-
 import { connect } from 'react-redux';
 
 function Counter({counter, dispatch}) {

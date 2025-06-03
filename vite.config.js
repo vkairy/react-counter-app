c07d21e4cd70de9f4c77285942fe0b1d5069a510
Valmir <vkairy@users.noreply.github.com>
2025-06-03 08:13:53 -0300

    fix: set basepath into vite.config.js to solve github-pages deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-counter-app/',
   loader: { '.js': '.jsx' },
   test: {
     globals: true,

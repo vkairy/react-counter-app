deae3770d92c815e846bd9bebf626013ade7e0d9
Valmir <vkairy@users.noreply.github.com>
2025-06-03 09:24:50 -0300

    feat: add version as metadata field into index.html (#28)
    feat: move to vite.config.ts instead of js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/react-counter-app/',
-  loader: { '.js': '.jsx' },
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
+  },
   test: {
     globals: true,
     environment: 'jsdom',
